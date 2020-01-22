1978f032b2821dab76c4a648b9910fbe067ee7dd
soupette <cyril.lpz@gmail.com>
2020-01-22 16:38:14 +0100

    Made loading a function
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- src/containers/App/init.js
+++ src/containers/App/init.js
@@ -1,11 +1,18 @@
-import { fromJS } from "immutable";
+import { fromJS } from 'immutable';
 
 // Simulate the pluginLoaded from 'strapi-admin/admin/src/app.js'
 function init({ initialState, appPlugins }) {
-  return initialState.update("plugins", () => {
+  return initialState.update('plugins', () => {
     return fromJS(
       Object.keys(appPlugins).reduce((acc, current) => {
-        acc[appPlugins[current].id] = appPlugins[current];
+        const registerPlugin = plugin => {
+          return plugin;
+        };
+
+        const currentPluginFn = appPlugins[current];
+        const plugin = currentPluginFn({ registerPlugin });
+
+        acc[plugin.id] = plugin;
 
         return acc;
       }, {}),
--- src/plugins/PluginA/index.js
+++ src/plugins/PluginA/index.js
@@ -2,21 +2,23 @@ import App from './containers/App';
 import Initializer from './containers/Initializer';
 import pluginId from './pluginId';
 
-const plugin = {
-  blockerComponent: null,
-  blockerComponentProps: {},
-  description: 'something',
-  icon: 'cog',
-  id: pluginId,
-  initializer: Initializer,
-  injectedComponents: [],
-  isReady: false,
-  leftMenuLinks: [],
-  leftMenuSections: [],
-  mainComponent: App,
-  name: 'pluginA',
-  preventComponentRendering: false,
-  trads: {},
-};
+export default strapi => {
+  const plugin = {
+    blockerComponent: null,
+    blockerComponentProps: {},
+    description: 'something',
+    icon: 'cog',
+    id: pluginId,
+    initializer: Initializer,
+    injectedComponents: [],
+    isReady: false,
+    leftMenuLinks: [],
+    leftMenuSections: [],
+    mainComponent: App,
+    name: 'pluginA',
+    preventComponentRendering: false,
+    trads: {},
+  };
 
-export default plugin;
+  return strapi.registerPlugin(plugin);
+};
