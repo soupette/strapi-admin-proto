9162ea7dabd048e6c8fdcc848cafa8586cf62b6c
soupette <cyril.lpz@gmail.com>
2020-01-22 16:14:51 +0100

    Simulate load plugin
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- src/plugins/PluginA/containers/App/index.js
+++ src/plugins/PluginA/containers/App/index.js
@@ -3,11 +3,31 @@
  * PluginA
  *
  */
-import React from "react";
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import { Switch, Route, useRouteMatch, Link } from 'react-router-dom';
+import EditView from '../EditView';
 // import PropTypes from 'prop-types';
 
 function PluginA() {
-  return <div>PluginA</div>;
+  let { url } = useRouteMatch();
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col md="4">
+          <ul>
+            <li>
+              <Link to={`${url}/edit-view`}>Go to EditView</Link>
+            </li>
+          </ul>
+        </Col>
+        <Switch>
+          <Route path={`${url}/edit-view`} component={EditView} />
+        </Switch>
+      </Row>
+    </Container>
+  );
 }
 
 PluginA.defaultProps = {};
